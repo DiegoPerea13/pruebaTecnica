4291d41039ce939776075e455ec3ef824437df62
DiegoPerea13 <diegoa.pereaa@uqvirtual.edu.co>
2024-03-26 19:58:52 -0500

    Configuration Db

--- Pizza/Infraestructure/Persistence/Configuration/DetalleVentaConfiguration.cs
+++ Pizza/Infraestructure/Persistence/Configuration/DetalleVentaConfiguration.cs
@@ -1,18 +1,28 @@
 using Domain.Objects.DetalleVenta;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Objects.Producto;
+using Domain.Objects.Venta;
 namespace Infraestructure.Persistence.Configuration;
 
-// public class DetalleVentaConfiguration : IEntityTypeConfiguration<DetalleVenta>
-// {
-//     public void Configure(EntityTypeBuilder<DetalleVenta> builder)
-//     {
-//         builder.ToTable("DetalleVentas");
-//         builder.HasKey(d => d.id);
+public class DetalleVentaConfiguration : IEntityTypeConfiguration<DetalleVenta>
+{
+    public void Configure(EntityTypeBuilder<DetalleVenta> builder)
+    {
+        builder.ToTable("DetalleVentas");
+        builder.HasKey(d => d.id);
 
-//         builder.Property(d => d.id).HasConversion(
-//             detalleId => detalleId.Value,    
-//             value => new DetalleVentaId(value)
-//         );
-//     }
-// }
+        builder.Property(d => d.id).HasConversion(
+            detalleId => detalleId.Value,
+            value => new DetalleVentaId(value)
+        );
+        builder.Property(d => d.idVenta).HasConversion(
+            ventaId => ventaId.Value,
+            value => new VentaId(value)
+        );
+        builder.Property(d => d.idProducto).HasConversion(
+            productoId => productoId.Value,
+            value => new ProductoId(value)
+        );
+    }
+}
--- Pizza/Infraestructure/Persistence/Configuration/VentaConfiguration.cs
+++ Pizza/Infraestructure/Persistence/Configuration/VentaConfiguration.cs
@@ -1,18 +1,22 @@
 using Domain.Objects.Venta;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using  Domain.Objects.Cliente;
 namespace Infraestructure.Persistence.Configuration;
 
-// public class VentaConfiguration : IEntityTypeConfiguration<Venta>
-// {
-//     public void Configure(EntityTypeBuilder<Venta> builder)
-//     {
-//         builder.ToTable("Ventas");
-//         builder.HasKey(v => v.id);
-
-//         builder.Property(v => v.id).HasConversion(
-//             ventaId => ventaId.Value,    
-//             value => new VentaId(value)
-//         );
-//     }
-// }
+public class VentaConfiguration : IEntityTypeConfiguration<Venta>
+{
+    public void Configure(EntityTypeBuilder<Venta> builder)
+    {
+        builder.ToTable("Ventas");
+        builder.HasKey(v => v.id);
+        builder.Property(v => v.id).HasConversion(
+            ventaId => ventaId.Value,    
+            value => new VentaId(value)
+        );
+        builder.Property(v => v.clienteId).HasConversion(
+            clienteId => clienteId.Value,    
+            value => new ClienteId(value)
+        );
+    }
+}
